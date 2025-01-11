44b81288e4436d80680f61602db8dd0ee2ef5014
Maciek Laptop <m13mor@gmail.com>
2025-01-11 01:12:22 +0100

    ugly but done

--- script.js
+++ script.js
@@ -11,43 +11,78 @@ document.addEventListener("DOMContentLoaded", () => {
     const fall = (i, data) => {
         return (fallcheck(data.daily[i].rain) + fallcheck(data.daily[i].snow));
     }
+    const date = (dt) => {
+        let s='';
+        const temp = new Date(dt * 1000);
+        s=s+temp.getDate()+'.'+temp.getMonth()+1;
+        return s;
+    }
     const apiKey = "2d365453b22c7818f34658ea43b65f15";
-    document.getElementById("getWeather").addEventListener("click", getWeather);
+    const button1 = document.getElementById("getWeather");
+    if(button1) {
+        button1.addEventListener("click", getWeather);
+    }
+    const button2 = document.getElementById("searchBtn");
+    if(button2) {
+        button2.addEventListener("click", findCity);
+    }
     function getWeather() {
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(fetchWeather);
+            navigator.geolocation.getCurrentPosition(pos);
         } else {
             alert("Twoja przeglądarka nie wspiera geolokacji!");
         }
     }
-
+    function pos(position) {
+        fetchWeather(position.coords.latitude, position.coords.longitude);
+    }
+    async function findCity() {
+        const searchBox = document.querySelector(".search input");
+        const city = searchBox.value;
+        const temp = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
+        try {
+            const res = await fetch(temp);
+            const data = await res.json();
+            if (res.ok) {
+                fetchWeather(data[0].lat, data[0].lon)
+            } else {
+                alert('Nie znaleziono miasta!');
+            }
+        } catch (error) {
+            console.error('Error fetching weather data:', error);
+        }
+    }
     function showPosition(position) {
         alert("Latitude: " + position.coords.latitude +
             "Longitude: " + position.coords.longitude);
     }
 
-    async function fetchWeather(position) {
-        const temp = `https://api.openweathermap.org/data/3.0/onecall?lat=${position.coords.latitude}&lon=${position.coords.longitude}&exclude=hourly,minutely&units=metric&lang=pl&appid=${apiKey}`;
+    async function fetchWeather(lat, lon) {
+        const temp = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=metric&lang=pl&appid=${apiKey}`;
         try {
             const res = await fetch(temp);
             const data = await res.json();
             if (res.ok) {
                 showWeather(data);
             } else {
-                alert('City not found. Please try again.');
+                alert('Nie znaleziono miasta!');
             }
         } catch (error) {
             console.error('Error fetching weather data:', error);
         }
     }
     function showWeather(data) {
-        for(let i = 0; i < 4; i++) {
+        var m = document.getElementById("main");
+        m.style.display = "none";
+        for(let i = 0; i < 8; i++) {
             if(i === 0) {
                 showWeatherCurr(data);
             } else {
                 showWeatherDay(data, i);
             }
         }
+        var s = document.getElementById("show");
+        s.style.display = "flex";
     }
     function showWeatherCurr(data) {
         const coll = document.getElementById("show").children;
@@ -68,6 +103,7 @@ document.addEventListener("DOMContentLoaded", () => {
         curr.querySelector("#humid").innerHTML = data.daily[i].humidity + "%";
         curr.querySelector("#rain").innerHTML = fall(i, data) + " mm";
         curr.querySelector("#dayIconImg").setAttribute("src", getIcon(data.daily[i].weather[0].main));
+        curr.querySelector("#dayTitle").innerHTML = date(data.daily[i].dt);
     }
     function getIcon(s) {
         for(const x of icons.keys()){
